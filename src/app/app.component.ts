dfb95f044a176a15fb30ee5090d52623daa323b3
Herman <hermanvasilkouskiy@gmail.com>
2024-12-08 21:04:49 +0300

    feat: update deps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { RouterOutlet } from '@angular/router';
 import WebApp from '@twa-dev/sdk';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
