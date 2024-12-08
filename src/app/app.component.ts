24765bb471236dd5cebcb5b1b131e1421082967b
Herman <hermanvasilkouskiy@gmail.com>
2024-12-08 20:49:51 +0300

    init: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import WebApp from '@twa-dev/sdk';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'test-telegram-app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    WebApp.showAlert('Hello world!');
+  }
 }
