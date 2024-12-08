ae5df51f878f0caae1e01e2cc614ca634410587a
Herman <hermanvasilkouskiy@gmail.com>
2024-12-08 21:10:16 +0300

    feat: add alert button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 import WebApp from '@twa-dev/sdk';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html'
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule],
+  templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
+export class AppComponent {
+  onAlert() {
     WebApp.showAlert('Hello world!');
   }
 }
